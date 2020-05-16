28cc6b29cd5a5a3a0fb1eb65a6869194e64da70a
nickson kley <nicksonkley14@gmail.com>
2020-05-16 11:15:10 -0400

    Initial Commit

---
+++ Escape_Mac/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Escape_Mac
+//
+//  Created by Nickson Kley Gonçalves Da Silva on 16/05/20.
+//  Copyright © 2020 Nickson Kley Gonçalves Da Silva. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
